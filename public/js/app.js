4156e7ce790b83c280d8d81c7b02f939aba77029
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-06-13 09:43:13 +0200

    added function delete with javascript

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+var deleteModal = $('#deleteModal');
+
+function deleteMsg(blog, id) {
+    $(this).click(function (event) {
+        event.preventDefault
+    });
+    deleteModal.modal('show');
+    $('.modal-body').text(blog);
+    $('.modal-footer .btn-danger').attr("id", id);
+}
+
+$('.modal-footer .btn-danger ').on("click",(ev)=>{
+    let id= ($(ev.target).attr("id"));
+    window.location.href="/delete-post/"+id;
+});
